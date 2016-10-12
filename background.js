dd2994f9614231f700893bf10fb76f5bb340b2d4
Tom Ross <tom@umpox.com>
2016-10-12 20:11:07 +0100

    Optimize function to utilise 'toggle'

--- background.js
+++ background.js
@@ -1,18 +1,8 @@
-var active = false;
 
 chrome.browserAction.onClicked.addListener(
     function(tab) { 
-        if (active === false) {
-            chrome.tabs.executeScript({
-                code: 'document.documentElement.classList.add("DoNotInteract")'
-            });
-            active = true;
-        }
-        else {
-            chrome.tabs.executeScript({
-                code: 'document.documentElement.classList.remove("DoNotInteract")'
-            });
-            active = false;
-        }
+        chrome.tabs.executeScript({
+            code: 'document.documentElement.classList.toggle("DoNotInteract")'
+        });
     }
 );
